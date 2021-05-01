5aa7535d43bed668fdcfe8e96d25a3ff43ee87b3
Alexander Andersson <alexanan@student.chalmers.se>
2021-05-01 19:56:13 +0200

    Added interface ICanNetworkConnection.

---
+++ CanDefinitions/ICanNetworkConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanDefinitions
+{
+    interface ICanNetworkConnection
+    {
+        void WriteMessage(CanMessage message);
+        IEnumerable<CanMessage> ReadMessages();
+
+        void OpenConnection(int baudrate);
+        void CloseConnection();
+
+        event EventHandler CanTransmissionStatusChanged;
+        event EventHandler<CanMessageReceivedEventArgs> CanMessageReceived;
+    }
+}
